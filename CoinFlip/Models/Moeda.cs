c830812583763df35c863ab87b1264319f7c5ec8
Guilherme Santos <guilherme.santos536@etec.sp.gov.br>
2024-06-19 15:43:22 -0300

    feat: added MVVM folder structure. Added MoedaViewModel, MoedaView and Moeda.cs

---
+++ CoinFlip/Models/Moeda.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlip.Models
+{
+    public class Moeda
+    {
+        //Encapsulamento "tradicional"
+        //Botão direito no nome do atributo, clique na primeira opção e depois encapsulate field.
+/*
+        private string lado;
+        public string Lado { get => lado; set => lado = value; }
+*/
+
+        //Criando uma propriedade
+        //Utilize o atalho "prop" dê enter e depois troque o nome da propriedade SEMPRE começando com maiúsculo
+        public string Lado { get; set; }
+
+        public void Sortear()
+        {
+            Lado = new Random().Next(2).ToString();
+            if (Lado == "0")
+            {
+                Lado = "cara";
+            }
+            else
+            {
+                Lado = "coroa";
+            }
+        }
+    }
+}
